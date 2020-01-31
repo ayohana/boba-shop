a0a348e047377e4743ed1709e543d7dadd82e2d9
Adela <adela.yohana@gmail.com>
2020-01-31 09:41:45 -0800

    Gather user input for multiple toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
+// Back-end Logic --------------------
 
+
+
+
+
+// Front-end Logic --------------------
+$(document).ready(function() {
+  $("form#drinkOrder").submit(function(event){
+    event.preventDefault();
+    $("input:checkbox[name=toppingOptions]:checked").each(function(){
+      var toppingSelected = $(this).val();
+      console.log(toppingSelected);
+    })
+  });
+
+  // var flavor = $("input:radio[name=flavor]:checked").val();
 });
