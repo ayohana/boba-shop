91ee74911980ef78838e8fbb5b5b1d0b0751b750
Adela <adela.yohana@gmail.com>
2020-01-31 09:57:03 -0800

    Display output of selected size

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,11 @@ $(document).ready(function() {
       var toppingSelected = $(this).val();
       $("#output").append(toppingSelected + "<br>");
     })
+
+    var sizeSelected = $("input:radio[name=sizeOptions]:checked").val();
+    $("#output").append(sizeSelected);
+
   });
 
-  // var flavor = $("input:radio[name=flavor]:checked").val();
+
 });
